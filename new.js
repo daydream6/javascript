9948e409fd893e40b926aa840bd3a10d9424e243
Doniyor <diplaydotcom@gmail.com>
2019-05-02 14:06:02 +0500

    first commit

---
+++ new.js
@@ -0,0 +1,7 @@
+
+
+	var num1 = prompt('Enter the first number');
+	var num2 = prompt('Enter the second number');
+	var sum = (num1 * 1) + (1 * num2);
+	document.write(num1 + num2 + sum);
+	alert(sum);
